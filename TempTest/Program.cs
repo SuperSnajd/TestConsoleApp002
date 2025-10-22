7d5b0b31b14724722d9d0ac6a40f864f0669880b
Mattias Nordenberg <mattias.nordenberg@tagmaster.com>
2025-10-22 14:56:18 +0200

    Update MCP configuration and remove temporary test files

--- TempTest/Program.cs
+++
@@ -1,8 +0,0 @@
-using Marten;
-
-var opts = new StoreOptions();
-var typeInfo = opts.AutoCreateSchemaObjects.GetType();
-Console.WriteLine($"Type: {typeInfo.FullName}");
-Console.WriteLine($"Namespace: {typeInfo.Namespace}");
-Console.WriteLine($"Assembly: {typeInfo.Assembly.GetName().Name}");
-
